7084d65d5a442b35a67118eb663677eda05adeff
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-11-02 21:21:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,7 +9,7 @@ class SongsController < ApplicationController
   get '/songs/new' do
     erb  :'/songs/new'
   end
-  
+
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/edit'
@@ -29,5 +29,16 @@ class SongsController < ApplicationController
     flash[:message] = "Successfully created song."
     redirect("/songs/#{@song.slug}")
   end
+
+  patch '/songs' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully created song."
+    redirect("/songs/#{@song.slug}")
+  end
+
 
 end
