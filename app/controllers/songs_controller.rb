3a5591e7da9ad5ea57af8bab2d0ef9759da5d466
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-11-02 21:08:01 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug/edit' do
-    @song = Song.all
+    @song = Song.find_by_slug(params[:slug])
     erb :'/songs/edit'
   end
 
